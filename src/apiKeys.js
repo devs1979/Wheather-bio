d528c40ccd349a4da3d8399fb10b76ea0aada00d
DIWESH KUMAAR TIWARI <diweshtiwari7417@gmail.com>
2023-07-26 22:26:13 +0530

    change api key

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "{Your  API Key Here}",
   base: "https://api.openweathermap.org/data/2.5/",
 };
