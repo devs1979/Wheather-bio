2f48397d717068d9ec1f56a39e0a1c31b738559f
DIWESH KUMAAR TIWARI <diweshtiwari7417@gmail.com>
2023-07-26 22:59:19 +0530

    api dal diye

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your  API Key Here}",
+  key: "1efeb0de6b5276c98bc06b03493f1a06",
   base: "https://api.openweathermap.org/data/2.5/",
 };
